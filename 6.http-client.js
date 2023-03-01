c4c292a2441f972e334584befea606e47a66a1cd
Viktor Sivulskiy <info@softmaker.kz>
2023-03-01 08:12:07 +0600

    http

--- 6.http-client.js
+++ 6.http-client.js
@@ -1,96 +1,60 @@
-// ## HTTP COLLECT(Задача 8 из 13)  
+// ## HTTP CLIENT(Задача 7 из 13)  
 
 //   Реализуйте программу, которая посылает HTTP GET запрос на URL, который вы  
-//   получите в качестве первого аргумента командной строки. Соберите все  
-//   данные с сервера(не только первое событие "data") и выведите две строки в
-//   консоль(stdout).  
+//   получите в качестве первого аргумента командной строки. Выведите  
+//   содержимое каждого события "data" из ответа(response) построчно в консоль
+//     (stdout).  
 
-//   Первая строка должна содержать количество символов, полученных с сервера.  
-//   А вторая должна непосредственно вывести эти символы.  
+//  ─────────────────────────────────────────────────────────────────────────────  
 
-// ## ИНФОРМАЦИЯ  
+//  ## ИНФОРМАЦИЯ  
 
-//   Вы можете решить эту задачу двумя путями:
+//   Для решения этой задачи Вам понадобится встроенный http модуль.  
 
-// 1) Собрать данные со всех событий "data" и последовательно добавлять их  
-//   для вывода в консоль. Используйте событие "end", для того чтобы понять,
-//   когда поток завершится, и Вы сможете вывести результат.  
+//   Документацию для http модуля Вы сможете найти набрав в браузере:
+// file:///Users/softmaker/.nvm/versions/node/v18.12.1/lib/node_modules/learnyounode/docs-nodejs/http.html  
 
-//   2) Использовать дополнительные библиотеки, для того чтобы абстрагироваться  
-//   от сложности сбора потоковых данных. Для этого есть две библиотеки(на  
-//   самом деле их больше!), которые решают эту задачу: bl(Buffer List) и
-//   concat-stream; выбор за Вами.  
+// http.get() это упрощенный метод для GET запросов, используйте его в своем
+// решении.Первым аргументом может быть URL, куда требуется отправить
+// запрос, вторым - функция обратного вызова.  
 
-//   < https://npmjs.com/bl> <https://npmjs.com/concat-stream>  
+//   В отличие от других функций обратного вызова, эта функция имеет следующую
+// сигнатуру:
 
-//   Для того чтобы установить библиотеку воспользуйтесь пакетным менеджером  
-//   для Node - npm.Просто наберите:  
+// function callback(response) { /* ... */ }  
 
-//      $ npm install bl  
+//   где response это объект типа Stream(поток).Вы можете интерпретировать  
+//   потоки как объекты, которые посылают события.Три из них наиболее
+// интересны: "data", "error" и "end".Вы можете подписаться на события  
+//   следующим образом:
 
-//   Таким образом Вы скачаете и установите последнюю версию этой библиотеки в  
-//   поддиректорию node_modules. Любая библиотека, установленная в эту  
-//   директорию может быть доступна в вашей программе с помощью конструкции
-//   require, без необходимости добавления префикса './':
+// response.on('data', function (data) { /* ... */ })  
 
-// const bl = require('bl')  
+//   Событие "data" посылается, когда часть данных становится доступной и может  
+//   быть обработана.Размер этих частей в основном зависит от источника
+// данных.
 
-//   Сначала Node будет искать библиотеку во встроенных модулях, а потом в  
-//   директории node_modules, где она и будет находиться.  
+//     Объект / поток response, который Вы можете получить из http.get() так же  
+//   имеет метод setEncoding().Если Вы передадите этому методу "utf8", то  
+//   событие "data" будет посылать данные строкового типа, нежели Node Buffer,
+//     который Вы должны будете постоянно конвертировать в строки.
 
-//   Если у Вас отсутствует соединение с интернетом, просто создайте директорию  
-//   node_modules и скопируйте туда директорию с библиотекой, которую Вы хотите  
-//   использовать из learnyounode:
+const http = require('http');
 
-// file:///Users/softmaker/.nvm/versions/node/v18.12.1/lib/node_modules/learn  
-// younode / node_modules / bl
-// file:///Users/softmaker/.nvm/versions/node/v18.12.1/lib/node_modules/learn  
-// younode / node_modules / concat - stream  
+const url = process.argv[2];
 
-//   Обе библиотеки bl и concat - stream могут принимать потоки и собирать из них  
-//   данные за Вас. Когда поток будет закончен, будет вызвана функция обратного  
-//   вызова переданными данными.
-
-//   response.pipe(bl(function (err, data) { /* ... */ }))
-//   or  
-//   response.pipe(concatStream(function (data) { /* ... */ }))  
-
-//   Примите во внимание то, что возможно Вам придется использовать
-//   data.toString() для конвертации данных из Buffer.  
-
-//   Документацию для обеих библиотек Вы сможете получить, набрав в браузере:
-
-// file:///Users/softmaker/.nvm/versions/node/v18.12.1/lib/node_modules/learn  
-// younode / docs / bl.html
-// file:///Users/softmaker/.nvm/versions/node/v18.12.1/lib/node_modules/learn  
-// younode / docs / concat - stream.html  
-
-'use strict'
-const http = require('http')
-
-http.get(process.argv[2], function (response) {
-    let rawData = '';
-    response.setEncoding('utf8')
-    response.on('data', chunk => rawData += chunk)
-    response.on('error', console.error)
-    response.on('end', () => {
-        console.log(rawData.length)
-        console.log(rawData)
+http.get(url, (res) => {
+    res.setEncoding('utf-8');
+    res.on('data', (chunk) => {
+        console.log(chunk);
     })
-}).on('error', console.error)
-
+})
 
 // 'use strict'
 // const http = require('http')
-// const bl = require('bl')
 
 // http.get(process.argv[2], function (response) {
-//     response.pipe(bl(function (err, data) {
-//         if (err) {
-//             return console.error(err)
-//         }
-//         data = data.toString()
-//         console.log(data.length)
-//         console.log(data)
-//     }))
-// })
+//     response.setEncoding('utf8')
+//     response.on('data', console.log)
+//     response.on('error', console.error)
+// }).on('error', console.error)
